24ca73c664f6dddc4ebbb3fda72a64035e3d4e23
Don734 <vip.don4ik@bk.ru>
2025-02-17 18:09:14 +0500

    upd web scraper

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Exports\ParseExport;
 use App\Http\Controllers\Controller;
+use App\Imports\CompanyImport;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
 
 class PageController extends Controller
@@ -31,35 +33,53 @@ class PageController extends Controller
         ]);
     }
 
+    public function getCompanies()
+    {
+        $links = [];
+        $companies = Excel::toArray(new CompanyImport, storage_path('app/public/table1.xlsx'));
+        foreach ($companies[0] as $company) {
+            $tin = (int) $company['stir'];
+            $ch = curl_init("https://orginfo.uz/search/all/?q={$tin}");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $html = curl_exec($ch);
+            curl_close($ch);
+            preg_match('/\/organization\/[A-Za-z0-9]+\//', $html, $url);
+            $links[] = $url[0] ?? $tin;
+        }
+        dd($links);
+    }
+
     public function parser()
     {
         $arr = [];
-        $address_tags = [
-            'Toshkent shahri'
-        ];
-        $ch = curl_init('https://orginfo.uz/organization/0731599e89ff/');
+        $ch = curl_init("https://orginfo.uz/organization/5124fd10f591/");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $html = curl_exec($ch);
-        // if ($html === false) {
-        //     echo "cURL Error: " . curl_error($ch);
-        // } else {
-        //     echo "Webpage content fetched successfully.";
-        // }
         curl_close($ch);
-        preg_match_all('/<span>\d{1,2}\.\d{1,2}\.\d{2,4}/', $html, $date_matches);
-        // preg_match('/\d{9}/', $html, $num_matches);
-        // preg_match('/Активный|Ликвидирована/', $html, $status);
-        preg_match_all('/<span>\d{5}\s/', $html, $num_matches1);
         $html = str_replace(["&nbsp;", "\u{A0}"], "", $html);
-        preg_match_all("/\d+,\d{2}/", $html, $amount);
-        preg_match_all("/\W+<\/span>/", $html, $words);
-        // preg_match_all("/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/", $html, $email);
-        foreach ($num_matches1 as $value) {
-            $val = str_replace(["<span>", " "], "", $value);
+        $html = str_replace("&#x27;", "'", $html);
+        $html = str_replace("&quot;", "\"", $html);
+        preg_match('/<span>\d{1,2}\.\d{1,2}\.\d{2,4}/', $html, $date);
+        preg_match('/ИНН-\d{9}/', $html, $tin);
+        preg_match('/Активный|Ликвидирована/', $html, $status);
+        preg_match_all('/\d{3,5}\s-\s\D+\W+</', $html, $num_matches1);
+        preg_match('/Об организации\s-\s\D+/', $html, $title);
+        preg_match("/\d+,\d{2}\s+(USD|UZS)\s+<\/span>/", $html, $amount);
+        preg_match("/q=\d+/", $html, $phone);
+        dd($html);
+        $arr[] = str_replace(["Об организации - ", ", ИНН-"], "", $title[0]);
+        $arr[] = str_replace("<span>", "", $date[0]);
+        $arr[] = str_replace("ИНН-", "", $tin[0]);
+        $arr[] = $status[0];
+        foreach ($num_matches1[0] as $value) {
+            $val = str_replace(["<span>", "<", "\n"], "", $value);
+            $val = preg_replace("/-\s+/", "", $val);
             $arr[] = $val;
         }
-        $arr = array_combine([0, 1], $arr[0]);
-        $arr[] = $amount[0][0];
+        $amount = str_replace([" ", "</span>", "\n"], "", trim($amount[0]));
+        $amount = str_replace(["USD", "UZS"], [" USD", " UZS"], $amount);
+        $arr[] = $amount;
+        $arr[] = str_replace("q=", "", $phone[0]);
         return Excel::download(new ParseExport($arr), 'file.xlsx', \Maatwebsite\Excel\Excel::XLSX);
     }
 }
--- resources/views/exports/parse.blade.php
+++ resources/views/exports/parse.blade.php
@@ -1,16 +1,30 @@
 <table>
     <thead>
         <tr>
+            <th>Название</th>
+            <th>Дата регистрации</th>
+            <th>Статус</th>
+            <th>ИНН</th>
+            <th>ОПФ</th>
             <th>СООГУ</th>
             <th>ОКЭД</th>
             <th>Уставный фонд</th>
+            <th>Телефон</th>
         </tr>
     </thead>
     <tbody>
+        @foreach ($data as $item)
         <tr>
-            @foreach ($data as $item)
-                <td>{{ $item }}</td>
-            @endforeach
+            <td>{{ $item[0] ?? "Отсутствует" }}</td>
+            <td>{{ $item[1] ?? "Отсутствует" }}</td>
+            <td>{{ $item[2] ?? "Отсутствует" }}</td>
+            <td>{{ $item[3] ?? "Отсутствует" }}</td>
+            <td>{{ $item[4] ?? "Отсутствует" }}</td>
+            <td>{{ $item[5] ?? "Отсутствует" }}</td>
+            <td>{{ $item[6] ?? "Отсутствует" }}</td>
+            <td>{{ $item[7] ?? "Отсутствует" }}</td>
+            <td>{{ $item[8] ?? "Отсутствует" }}</td>
         </tr>
+        @endforeach
     </tbody>
 </table>
